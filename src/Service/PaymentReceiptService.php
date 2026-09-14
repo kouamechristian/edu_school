b49db8fe6c81422af6ec7c564831c9c703258f8c
kouamechristian <102165926+kouamechristian@users.noreply.github.com>
2026-09-14 11:26:31 +0000

    Update PaymentReceiptService.php

--- src/Service/PaymentReceiptService.php
+++ src/Service/PaymentReceiptService.php
@@ -51,7 +51,7 @@ class PaymentReceiptService
         $html = $this->twig->render('payment/receipt.pdf.html.twig', [
             'payment' => $lines[0],
             'receipt_number' => $payment->getReceiptNumber() ?? $payment->getPaymentNumber(),
-            'receipt_amount' => $paidAmount,
+            'receipt_amount' => $amount,
             'student' => $student,
             'school' => $school,
             'logo_data' => $this->buildLogoData($school),
